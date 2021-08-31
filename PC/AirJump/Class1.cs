9574815243c4f482385445af4fb7e24351150478
fchb1239 <fchb1239.dk@gmail.com>
2021-08-31 18:41:46 +0200

    Update Class1.cs

--- PC/AirJump/Class1.cs
+++ PC/AirJump/Class1.cs
@@ -1,4 +1,4 @@
-//WARNING: THIS CODE IS WORSE THAN YANDERE SIMULATOR CODE! DON'T READ THIS TO LEARN ANYTHING, IT'S BAD!
+//Code kinda shitty
 using System;
 using System.Collections.Generic;
 using HarmonyLib;
@@ -23,7 +23,7 @@ namespace AirJump
 {
     [BepInPlugin("org.fchb1239.gorillagame.AirJump", "AirJump", "0.6.9")]
 
-    public class MonkePlugin : BaseUnityPlugin
+    public class Plugin : BaseUnityPlugin
     {
         static bool enabled = true; //It's for ComputerInterface.
         private void Awake()
@@ -45,8 +45,7 @@ namespace AirJump
         [HarmonyPatch("Update", MethodType.Normal)]
         private class AirJump_Patch
         {
-            //Gotta make em' all static since the Postfix is forcing me to.
-            static Vector3 scale = new Vector3(0.0125f, 0.25f, 0.335f);
+            static Vector3 scale = new Vector3(0.0125f, 0.28f, 0.3825f);
             static bool gripDown_left;
             static bool gripDown_right;
             static bool once_left;
@@ -54,10 +53,8 @@ namespace AirJump
             static bool once_left_false;
             static bool once_right_false;
             static bool once_networking;
-            static int[] int_jump_left_network = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            static int[] int_jump_right_network = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            static GameObject[] jump_left_network = new GameObject[] { null, null, null, null, null, null, null, null, null };
-            static GameObject[] jump_right_network = new GameObject[] { null, null, null, null, null, null, null, null, null };
+            static GameObject[] jump_left_network = new GameObject[9999];
+            static GameObject[] jump_right_network = new GameObject[9999];
             static GameObject jump_left_local = null;
             static GameObject jump_right_local = null;
 
@@ -183,64 +180,30 @@ namespace AirJump
                 if (eventCode == (byte)PhotonEventCodes.left_jump_photoncode)
                 {
                     object[] data_left = (object[])eventData.CustomData; //Array
-                    try
-                    {
-                        for (int i = 0; i < jump_left_network.Length; i++)
-                        {
-                            int_jump_left_network[i] = eventData.Sender;
-                            jump_left_network[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                            jump_left_network[i].GetComponent<Renderer>().material.SetColor("_Color", Color.black);
-                            jump_left_network[i].transform.localScale = scale;
-                            jump_left_network[i].transform.position = (Vector3)data_left[0]; //Sets position.
-                            jump_left_network[i].transform.rotation = (Quaternion)data_left[1]; //Sets rotation.
-                            i++;
-                        }
-                    }
-                    catch (Exception e) { Console.WriteLine(e); }
+                    jump_left_network[eventData.Sender] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    jump_left_network[eventData.Sender].GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                    jump_left_network[eventData.Sender].transform.localScale = scale;
+                    jump_left_network[eventData.Sender].transform.position = (Vector3)data_left[0]; //Sets position.
+                    jump_left_network[eventData.Sender].transform.rotation = (Quaternion)data_left[1]; //Sets rotation.
                 }
                 else if (eventCode == (byte)PhotonEventCodes.right_jump_photoncode)
                 {
                     object[] data_right = (object[])eventData.CustomData; //Array
-                    try
-                    {
-                        for (int i = 0; i < jump_right_network.Length; i++)
-                        {
-                            int_jump_right_network[i] = eventData.Sender;
-                            jump_right_network[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                            jump_right_network[i].GetComponent<Renderer>().material.SetColor("_Color", Color.black);
-                            jump_right_network[i].transform.localScale = scale;
-                            jump_right_network[i].transform.position = (Vector3)data_right[0]; //Sets position.
-                            jump_right_network[i].transform.rotation = (Quaternion)data_right[1]; //Sets rotation.
-                            i++;
-                        }
-                    }
-                    catch (Exception e) { Console.WriteLine(e); }
+                    jump_right_network[eventData.Sender] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    jump_right_network[eventData.Sender].GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                    jump_right_network[eventData.Sender].transform.localScale = scale;
+                    jump_right_network[eventData.Sender].transform.position = (Vector3)data_right[0]; //Sets position.
+                    jump_right_network[eventData.Sender].transform.rotation = (Quaternion)data_right[1]; //Sets rotation.
                 }
                 else if (eventCode == (byte)PhotonEventCodes.left_jump_deletion)
                 {
-                    int i = 0;
-                    foreach (int int_net in int_jump_left_network)
-                    {
-                        if (int_net == eventData.Sender)
-                        {
-                            GameObject.Destroy(jump_left_network[i]);
-                            jump_left_network[i] = null;
-                        }
-                        i++;
-                    }
+                    GameObject.Destroy(jump_left_network[eventData.Sender]);
+                    jump_left_network[eventData.Sender] = null;
                 }
                 else if (eventCode == (byte)PhotonEventCodes.right_jump_deletion)
                 {
-                    int i = 0;
-                    foreach (int int_net in int_jump_right_network)
-                    {
-                        if (int_net == eventData.Sender)
-                        {
-                            GameObject.Destroy(jump_right_network[i]);
-                            jump_right_network[i] = null;
-                        }
-                        i++;
-                    }
+                    GameObject.Destroy(jump_right_network[eventData.Sender]);
+                    jump_right_network[eventData.Sender] = null;
                 }
             }
         }
