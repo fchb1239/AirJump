8a82200dff9d871b107297352c8e4662a83bca09
fchb1239 <fchb1239.dk@gmail.com>
2022-04-08 23:39:12 +0200

    working on hmm support but can't be asked lmao

---
+++ ComputerInterface/AirJumpEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ComputerInterface.Interfaces;
+
+namespace AirJump.ComputerInterface
+{
+    class AirJumpEntry : IComputerModEntry
+    {
+        public string EntryName => "AirJump";
+
+        // This is the first view that is going to be shown if the user select you mod
+        // The Computer Interface mod will instantiate your view 
+        public Type EntryViewType => typeof(AirJumpView);
+    }
+}
