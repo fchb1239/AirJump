482bd05317c1a9614214add5a2f7769bcaf7a678
fchb1239 <fchb1239.dk@gmail.com>
2022-04-15 21:11:11 +0200

    update version number

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.fchb1239.gorillatag.airjump";
         public const string Name = "AirJump";
-        public const string Version = "1.0.2";
+        public const string Version = "1.0.3";
     }
 }
