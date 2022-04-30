34e8d350f56e363620a8187786d26cbb04b6bbb3
fchb1239 <fchb1239.dk@gmail.com>
2022-04-30 13:18:54 +0200

    new stuff

---
+++ Data/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace AirJump.Data
+{
+    [System.Serializable]
+    class Settings
+    {
+        public int sizeIndex;
+        public int matIndex;
+        public bool otherCollisions;
+    }
+}
---
+++ Logging/AJLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirJump.Logging
+{
+    class AJLog
+    {
+        public static void Log(string content)
+        {
+            Console.WriteLine($"AirJump - {content} - {DateTime.Now}");
+        }
+    }
+}
